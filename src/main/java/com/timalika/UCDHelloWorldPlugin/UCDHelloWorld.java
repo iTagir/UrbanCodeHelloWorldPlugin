69fb95069c914a89f64a8f6a1facedda8e4745c0
Tagir <tagir@timalika.co.uk>
2017-02-21 11:43:05 +0000

    initial commit

---
+++ src/main/java/com/timalika/UCDHelloWorldPlugin/UCDHelloWorld.java
@@ -0,0 +1,49 @@
+package com.timalika.UCDHelloWorldPlugin;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Hello world!
+ *
+ */
+public class UCDHelloWorld 
+{
+    public static void main( String[] args )
+    {
+    	String workingDir = System.getProperty("user.dir");
+    	System.out.println("Working Directory : " + workingDir);
+    	String propsFilePath = args[0];
+    	Properties prop = new Properties();
+    	InputStream input = null;
+
+    	try {
+
+    		input = new FileInputStream(propsFilePath);
+    		
+
+    		// load a properties file
+    		prop.load(input);
+
+    		// get the property value and print it out
+    		System.out.println("Message : " + prop.getProperty("message"));
+    		
+
+    	} catch (IOException ex) {
+    		ex.printStackTrace();
+    		System.exit(1);
+    	} finally {
+    		if (input != null) {
+    			try {
+    				input.close();
+    			} catch (IOException e) {
+    				e.printStackTrace();
+    				System.exit(1);
+    			}
+    		}
+    	}
+    	System.exit(0);
+    }
+}
---
+++ src/test/java/com/timalika/UCDHelloWorldPlugin/UCDHelloWorldTest.java
@@ -0,0 +1,38 @@
+package com.timalika.UCDHelloWorldPlugin;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class UCDHelloWorldTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public UCDHelloWorldTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( UCDHelloWorld.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
