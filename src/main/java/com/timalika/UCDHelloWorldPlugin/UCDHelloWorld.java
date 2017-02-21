f388d3703ccb6b5528dc9273ce7d582ed6d4c881
Tagir <tagir@timalika.co.uk>
2017-02-21 13:28:17 +0000

    comments and updates

--- src/main/java/com/timalika/UCDHelloWorldPlugin/UCDHelloWorld.java
+++ src/main/java/com/timalika/UCDHelloWorldPlugin/UCDHelloWorld.java
@@ -6,31 +6,28 @@ import java.io.InputStream;
 import java.util.Properties;
 
 /**
- * Hello world!
+ * Hello world plugin for UrbanCode deploy!
  *
  */
 public class UCDHelloWorld 
 {
     public static void main( String[] args )
     {
+    	//get the working directory first
     	String workingDir = System.getProperty("user.dir");
     	System.out.println("Working Directory : " + workingDir);
+    	
     	String propsFilePath = args[0];
     	Properties prop = new Properties();
     	InputStream input = null;
 
     	try {
-
-    		input = new FileInputStream(propsFilePath);
-    		
-
+    		input = new FileInputStream(propsFilePath);    		
     		// load a properties file
     		prop.load(input);
-
     		// get the property value and print it out
     		System.out.println("Message : " + prop.getProperty("message"));
     		
-
     	} catch (IOException ex) {
     		ex.printStackTrace();
     		System.exit(1);
